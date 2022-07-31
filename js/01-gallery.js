3ea5cf59c42803d20690875ffe409aa4bc4e3612
TaniaSem <semenchuktetiana@gmail.com>
2022-07-31 23:41:31 +0300

    finish first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imgCardMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imgCardMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+      `;
+    })
+    .join(" ");
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  const imgNodeName = evt.target.nodeName === "IMG";
+  if (!imgNodeName) {
+    return;
+  } else {
+    const originalImg = evt.target.dataset.source;
+
+    createBigImgMarkup(originalImg).show();
+  }
+}
+
+function createBigImgMarkup(src) {
+  const instance = basicLightbox.create(
+    `
+    <img src="${src}" width="1280" height="800">
+`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscClick);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscClick);
+      },
+    }
+  );
+  function onEscClick(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+
+  return instance;
+}
