9bf01f2fa06fc4436c0585e66a8915fc800d0a74
TaniaSem <semenchuktetiana@gmail.com>
2022-08-01 22:05:41 +0300

    finish second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const imgCardMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imgCardMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+      `;
+    })
+    .join(" ");
+}
+
+const gallery = new SimpleLightbox(".gallery  a", {
+  scrollZoom: false,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  const imgNodeName = evt.target.nodeName;
+  if (!imgNodeName) {
+    return;
+  } else {
+    gallery;
+  }
+}
